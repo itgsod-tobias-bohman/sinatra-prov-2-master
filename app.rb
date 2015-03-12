da698cc29fe5ea565410cdd6a82864284af6ee2c
Tobias Bohman <tobias.bohman@elev.it-gymnasiet.se>
2015-03-12 10:23:25 +0100

    added ground base

--- app.rb
+++ app.rb
@@ -1,7 +1,23 @@
 class App < Sinatra::Base
 
   get '/' do
+    @movies = Movie.all(:order => [:name.asc])
+    @cinemas = Cinema.all(:order => [:name.asc])
+    slim :index
+  end
 
+  get '/cinema/:cinema' do |cinema|
+    @cinema = Cinema.first(:name => "#{cinema}")
+
+    @showings = Showing.all(:cinema_id => "#{@cinema.id}", :fields => [:movie_id], :unique => true, :order => [:movie_id.asc])
+
+    @showings.inspect
+    #slim :cinema
+  end
+
+  get '/movie/:movie' do |movie|
+    @movie = Movie.first(:name => "#{movie}")
+    slim :movie
   end
 
 end
